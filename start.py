090dcc36d256fadee67b243448e0cf01741dc1d1
wayne1000901103 <140511989+wayne1000901103@users.noreply.github.com>
2025-02-09 20:51:34 +0800

    提交檔案

---
+++ start.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+import os
+
+def run_blue_screen():
+    os.system("start CMD-blue-screen.bat")
+
+def run_crash():
+    os.system("start CMD-Crash.bat")
+
+def run_popups():
+    os.system("start CMD-unlimited-pop-ups.bat")
+
+def run_vbs():
+    os.system("start VBS-that-can’t-be-turned-off.vbs")
+
+root = tk.Tk()
+root.title("惡搞工具")
+
+# 創建按鈕
+btn_blue_screen = tk.Button(root, text="藍屏", command=run_blue_screen)
+btn_blue_screen.pack(pady=10)
+
+btn_crash = tk.Button(root, text="崩潰系統", command=run_crash)
+btn_crash.pack(pady=10)
+
+btn_popups = tk.Button(root, text="無限彈窗", command=run_popups)
+btn_popups.pack(pady=10)
+
+btn_vbs = tk.Button(root, text="無法關閉的VBS", command=run_vbs)
+btn_vbs.pack(pady=10)
+
+# 啟動 GUI
+root.mainloop()
